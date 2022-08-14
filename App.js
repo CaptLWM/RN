0cedbdc98729f830619a898f8ebfd88b1fa5e8c1
CaptLWM <wonmin275@gmail.com>
2022-08-14 16:15:46 +0900

    RN 2.8 count

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import {SafeAreaView, View, Text, Button} from 'react-native';
+import {SafeAreaView, View, Text, Button, StyleSheet} from 'react-native';
 import Box from './components/Box';
+import Counter from './components/Counter';
 import Greeting from './components/Greeting';
 
 const App = () => {
-  // 훅 사용 규칙
-  // 1. 컴포넌트의 최상위 레벨에서 사용
-  // 2. Custom Hook 사용 가능
-  // 3. 클래스에선 사용 불가
-  const [visible, setVisible] = React.useState(true);
-  const onPress = () => {
-    setVisible(!visible);
+  const [count, setCount] = React.useState(0);
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+  const onDecrease = () => {
+    setCount(count - 1);
   };
 
   return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-      {/* <Greeting name="Props" /> */}
-      {visible && <Box rounded size="large" color="blue" />}
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+    // flex :1 => 자신이 위치한 곳의 영역을 모두 차지하겠다는 의미
+  },
+});
 
 export default App;
