1031486e6c9a800f5fc68b435e4562b039d41106
CaptLWM <wonmin275@gmail.com>
2022-08-14 15:52:40 +0900

    RN 2.7

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button} from 'react-native';
 import Box from './components/Box';
 import Greeting from './components/Greeting';
 
 const App = () => {
+  // 훅 사용 규칙
+  // 1. 컴포넌트의 최상위 레벨에서 사용
+  // 2. Custom Hook 사용 가능
+  // 3. 클래스에선 사용 불가
+  const [visible, setVisible] = React.useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
+
   return (
     <SafeAreaView>
-      <View>
-        {/* <Greeting name="Props" /> */}
-        <Box rounded size="large" color="blue" />
-      </View>
+      <Button title="토글" onPress={onPress} />
+      {/* <Greeting name="Props" /> */}
+      {visible && <Box rounded size="large" color="blue" />}
     </SafeAreaView>
   );
 };
